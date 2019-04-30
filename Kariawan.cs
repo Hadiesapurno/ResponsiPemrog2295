c158d56e69d25ac07ca41fb9f053c666c0d9baad
Hadiesapurno <iqbal.purnomo@students.amikom.ac.id>
2019-04-30 19:11:07 +0700

    full

---
+++ Kariawan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2295
+{
+    class Kariawan {
+        public string nama { get; set; }
+        public string nik { get; set; }
+        public int gajih { get; set; } 
+
+       
+
+        public Kariawan (string Nama, string NIK, int Gajih) 
+        {
+            if (Gajih < 0) { gajih = 0; } else { gajih = Gajih; };
+            nama = Nama;
+            nik = NIK;
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrog2295
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==================================================");
+            Console.WriteLine("NAMA\t\t|\tNIK\t|\tGAJI");
+            Console.WriteLine("==================================================");
+            Kariawan org1 = new Kariawan("Sukijan", "34567", 1000000);
+            cetak(org1);
+            Kariawan org2 = new Kariawan("Sukit", "2571", 1500000);
+            cetak(org2);
+            NaikGajih(org1);
+            NaikGajih(org2);
+            Console.WriteLine("==================================================");
+            Console.WriteLine("NAMA\t\t|\tNIK\t|\tGAJI");
+            Console.WriteLine("==================================================");
+            cetak(org1);
+            cetak(org2);
+            Console.ReadKey();
+        }
+
+
+        static void cetak(Kariawan org) {
+            Console.WriteLine("{0}\t\t|\t{1}\t|\t{2}",org.nama,org.nik,org.gajih);
+        }
+
+        static void NaikGajih( Kariawan uang) {
+            int gajihKariawan = uang.gajih;
+            int hasil = 0;
+            hasil = (gajihKariawan / 100) * 10;
+            uang.gajih = hasil + gajihKariawan;
+        }
+
+    }
+}
